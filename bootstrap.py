c25a5a619e6884f596168becec4ecfa25f2f484c
Angela Dang Atkinson <angelakdang@gmail.com>
2023-05-18 15:14:04 -0600

    Moved utility functions out

--- bootstrap.py
+++ bootstrap.py
@@ -1,60 +1,13 @@
-import random
-from dataclasses import dataclass
-from typing import List, Tuple
-
 import arrow
-import yaml
 from cognite.client.data_classes import TimeSeries
 
 from auth import get_cognite_client
+from utils import generate_random_data, get_heat_exchanger_data
 
 client = get_cognite_client()
 
-
-# Define the data class for HeatExchangers
-@dataclass
-class HeatExchanger:
-    name: str
-    coolWatSupplyTemp: str
-    coolWatReturnTemp: str
-    processSupplyTemp: str
-    processReturnTemp: str
-    coolWatFlow: str
-    designArea: int
-    subFlows: int
-    enabler: str = None
-    enablerLimit: int = 0
-
-
-def generate_random_data(
-    start: arrow.Arrow = None,
-    end: arrow.Arrow = None,
-    granularity: int = 1,
-    value_range: Tuple[int, int] = (0, 1),
-) -> List[Tuple[float, float]]:
-    if start is None:
-        start = arrow.utcnow().shift(hours=-24)
-    if end is None:
-        end = arrow.utcnow()
-
-    output = []
-
-    current_time = start.floor("minute")
-    while current_time < end:
-        timestamp = current_time.timestamp() * 1000
-        value = random.uniform(*value_range)
-        output.append((int(timestamp), value))
-        current_time = current_time.shift(minutes=granularity)
-
-    return output
-
-
 # Load the YAML file into a list of data class instances
-heat_exchangers = []
-with open("data.yaml", "r") as file:
-    data = yaml.safe_load(file)
-    for d in data:
-        heat_exchangers.append(HeatExchanger(**d))
+heat_exchangers = get_heat_exchanger_data()
 
 # Generate timeseries data in CDF
 ts_xids = []
